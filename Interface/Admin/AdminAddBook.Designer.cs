5e52c2abb776e4e13447ec8180633d38b6d6e49f
hxq5201314 <3166586463@qq.com>
2026-09-02 16:54:00 +0800

    规范事件处理函数命名为 Button2_Click
    将 button2 的 Click 事件处理方法由 button2_Click 修改为 Button2_Click，统一了事件处理函数的命名规范。

--- Interface/Admin/AdminAddBook.Designer.cs
+++ Interface/Admin/AdminAddBook.Designer.cs
@@ -180,7 +180,7 @@
             this.button2.TabIndex = 3;
             this.button2.Text = "取消";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
             // 
             // label7
             // 
